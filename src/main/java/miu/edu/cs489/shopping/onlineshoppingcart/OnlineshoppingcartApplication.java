0570de8dfcd9535d3c94530d567a64acf493a64b
sophearyrin1 <srin@miu.edu>
2023-10-30 18:46:21 -0500

    inital project

---
+++ src/main/java/miu/edu/cs489/shopping/onlineshoppingcart/OnlineshoppingcartApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs489.shopping.onlineshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshoppingcartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miu/edu/cs489/shopping/onlineshoppingcart/OnlineshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs489.shopping.onlineshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshoppingcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
